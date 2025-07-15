0b176c3beee494792d9158dda0cbc1b3ed48ab15
rohan27795 <rohanpanga@gmail.com>
2025-07-15 20:03:41 +0530

    Time: 999 ms (42.2%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,10 @@
+SELECT
+  contest_id,
+  round(
+    count(distinct user_id) * 100.0 / (select count(user_id) from users),
+    2
+  ) as percentage
+from register 
+group by contest_id
+order by percentage desc, contest_id asc
+
