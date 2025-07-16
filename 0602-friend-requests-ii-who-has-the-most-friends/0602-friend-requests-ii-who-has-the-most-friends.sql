b66e084be0e347f12282842d3664d7bd26dd4f81
rohan27795 <rohanpanga@gmail.com>
2025-07-16 13:36:48 +0530

    Time: 269 ms (97.81%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -2,7 +2,7 @@
 select id ,count(id) as num from
 (select requester_id as id from RequestAccepted 
 union all 
-select accepter_id as id from RequestAccepted) temp
+select accepter_id as id from RequestAccepted) as temp
 group by id
 order by num desc
 limit 1
