0492a3bd9dc20317299c1d4c7f7c06870a48d9e2
rohan27795 <rohanpanga@gmail.com>
2025-09-06 19:28:31 +0530

    Time: 61 ms (75.82%), Space: 12.6 MB (73.83%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,48 @@
+class Solution(object):
+    def findTargetSumWays(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        total = sum(nums)
+
+        # Not possible case
+        if total < abs(target) or (total + target) % 2 != 0:
+
+            return 0
+
+        subset_sum = (total + target) // 2
+
+        # Count subsets with sum = subset_sum
+        return self.countSubsets(nums, subset_sum)
+
+    def countSubsets(self, nums, target_sum):
+        n = len(nums)
+        dp = [[0] * (target_sum + 1) for _ in range(n + 1)]
+
+        # Base case: 1 way to make sum=0 (pick nothing)
+        for i in range(n + 1):
+            dp[i][0] = 1
+
+        for i in range(1, n + 1):
+            for j in range(target_sum + 1):
+                if nums[i - 1] <= j:
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+
+        return dp[n][target_sum]
+
+
+    
+
+
+        
+
+        
+            
+            
+
+        
+        
