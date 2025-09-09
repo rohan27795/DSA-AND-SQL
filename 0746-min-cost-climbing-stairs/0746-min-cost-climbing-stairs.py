661f9a8e503c0ed90e621171a340c3dc68cdb0a1
rohan27795 <rohanpanga@gmail.com>
2025-09-09 20:32:01 +0530

    Time: 2 ms (78.22%), Space: 12.7 MB (26.86%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -7,6 +7,9 @@ class Solution(object):
         n = len(cost)
         t = [0] * (n+1)
 
+        t[0] = 0
+        t[1] = 0
+
         for i in range(2,n+1):
             
             t[i] = min(t[i-1]+cost[i-1],t[i-2]+cost[i-2])
