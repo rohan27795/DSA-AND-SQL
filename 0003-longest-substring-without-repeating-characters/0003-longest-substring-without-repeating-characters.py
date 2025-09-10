07f06bf9f54647305acddcebba2b2aed0c2e6bc4
rohan27795 <rohanpanga@gmail.com>
2025-09-10 17:00:22 +0530

    Time: 32 ms (25.86%), Space: 12.6 MB (71.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,16 +4,31 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        charset=set()
-        l=0
-        res=0
-        for r in range(len(s)):
-            while s[r] in charset:
-                charset.remove(s[l])
-                l+=1
-            charset.add(s[r])
-            res = max(res,r-l+1)
-        return res
+        n = len(s)
+        i = 0
+        j = 0
+        max_len = 0
+        freq = {}
         
-    
+        while j<n:
+            freq[s[j]] = freq.get(s[j],0) +1
+            
+            if len(freq) == j-i+1:
+                max_len = max(max_len,j-i+1)
+                j +=1
+
+            else: 
+                while len(freq) < j-i+1:
+                    freq[s[i]] -=1
+
+                    if freq[s[i]] == 0:
+                        del freq[s[i]]
+                    i +=1
+                j +=1
+        
+        return max_len
+
+
+
+
         
