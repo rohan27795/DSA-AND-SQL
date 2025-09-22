6b8721854d50089cb71989233288a87ebbd17887
rohan27795 <rohanpanga@gmail.com>
2025-09-22 19:06:33 +0530

    Time: 62 ms (12.63%), Space: 12.5 MB (37.71%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        candidates.sort()
+        subset = []
+        def dfs(i,total):
+            if total == target:
+                res.append(subset[:])
+                return
+            
+            if i >= len(candidates) or total > target:
+                return 
+                
+            subset.append(candidates[i])
+            dfs(i+1,total + candidates[i]) #multiple value
+
+            subset.pop()
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i +=1
+            dfs(i+1,total)   #exclude
+
+        dfs(0,0)
+        return res
+        
+
+                
