501760936bc683b2ef19f219b07f48fa8519f609
rohan27795 <rohanpanga@gmail.com>
2025-09-30 16:33:02 +0530

    Time: 3 ms (62.56%), Space: 12.5 MB (98.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -7,34 +7,31 @@ class Solution(object):
         """
         if len(nums1) > len(nums2):
             nums1,nums2 = nums2,nums1
-        n1,n2 = len(nums1),len(nums2)
-        low = 0
-        high = n1
-        while low <= high:
-            mid1 = (high+low) // 2
-            mid2 = (n1+n2+1) //2 - mid1
+        n1 = len(nums1)
+        n2 = len(nums2)
+        l = 0
+        r = n1
+        while l <= r:
+            mid1 = (l+r) // 2
+            mid2 = (n1+n2+1)//2 - mid1
 
             l1 = float('-inf') if mid1 == 0 else nums1[mid1-1]
-            r1 = float('inf')  if mid1 == n1 else nums1[mid1]
+            r1 = float('inf') if mid1 == n1 else nums1[mid1]
             l2 = float('-inf') if mid2 == 0 else nums2[mid2-1]
-            r2 = float('inf')  if mid2 == n2 else nums2[mid2]
+            r2 = float('inf') if mid2 == n2 else nums2[mid2]
 
             if l1 <= r2 and l2 <= r1:
                 if (n1+n2) % 2 == 0:
-                    return (max(l1,l2) + min(r1,r2)) / 2.0   #even
+                    return (max(l1,l2) + min(r1,r2)) / 2.0
                 else:
-                    return max(l1,l2)                               #odd
+                    return max(l1,l2)
+
             elif l1 > r2:
-                high = mid1-1
+                r = mid1 - 1
+
             else:
-                low = mid1+1
-
-        return 0.0
-
-
-
-
-            
-
+                l = mid1 + 1
 
         
+    
+
