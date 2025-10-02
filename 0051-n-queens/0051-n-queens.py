c6d5f5ae1676935691e3150478b51f020054f828
rohan27795 <rohanpanga@gmail.com>
2025-10-02 22:51:27 +0530

    Time: 51 ms (33.36%), Space: 12.5 MB (76.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -4,41 +4,48 @@ class Solution(object):
         :type n: int
         :rtype: List[List[str]]
         """
-        board = [["."] * n for _ in range(n)]  # initalize empty chess board
+        board = [["."] * n for _ in range(n)]
         res = []
-
-        def is_safe(board,row,col,n):
-
+        def issafe(board,row,col,n):
             for i in range(row):
-                if board[i][col] == "Q":  # check upper column
+                if board[i][col] == "Q":
                     return False
 
             i,j = row-1,col-1
             while i >= 0 and j >= 0:
-                if board[i][j] == "Q":     #check upper left diagonal
+                if board[i][j] == "Q":
                     return False
                 i -=1
                 j -=1
-            i,j = row-1,col+1      
+
+            i,j = row-1,col+1
             while i >= 0 and j < n:
-                if board[i][j] == "Q":      #check upper right diagonal
+                if board[i][j] == "Q":
                     return False
                 i -=1
                 j +=1
-
             return True
 
         def Nqueen(board,row,n,res):
             if row == n:
                 res.append(["".join(r) for r in board])
+
             for col in range(n):
-                if is_safe(board,row,col,n):
+                if issafe(board,row,col,n):
                     board[row][col] = "Q"
-                    Nqueen(board,row+1,n,res)   #recuse every row
-                    board[row][col] = "."       #backtrack
+                    Nqueen(board,row+1,n,res)
+                    board[row][col] = "."
 
         Nqueen(board,0,n,res)
         return res
+
+
+
+                
+
+
+
+
 
             
         
